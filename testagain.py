471aee9296720fb810a19a3f19275d39d8e556db
shaunikm <shaunik.musukula@gmail.com>
2020-07-30 11:18:21 -0700

    my second commit

--- testagain.py
+++ testagain.py
@@ -1,5 +1,2 @@
-import os, time
-
-print('cool')
-time.sleep(1)
-os.system('cls')
+print(len('The Ansi escape codes let you set the color of the text-background the same way it lets you set the color of the foregrond.'))
+print(str(100/len('The Ansi escape codes let you set the color of the text-background the same way it lets you set the color of the foregrond.')))
