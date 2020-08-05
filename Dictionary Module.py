e2b882b4d344ab843826c1b00f7f0bd50691b660
shaunikm <shaunik.musukula@gmail.com>
2020-08-05 14:16:56 -0700

    Module/Library for dictionary

---
+++ Dictionary Module.py
@@ -0,0 +1,44 @@
+#this function helps 'unlock and lock' dictionaries in python
+#you would need to enter a boolean value for the lock and unlock functions
+
+#the class that holds all the commands
+class dict_control:
+    def __init__(self, d):
+        self.d = d
+        self.b = True
+        self.j = d
+
+    def unlock(self):
+        self.b = True
+
+    def lock(self):
+        self.b = False
+        self.d = {}
+
+    def access(self, *num):
+            if not self.b:
+                return '\u001b[31mAccess Denied\u001b[0m'
+            else:
+                self.d = self.j
+                return self.d
+
+    def stat(self):
+       if not self.b:
+           return '\u001b[31mLOCKED\u001b[0m'
+       else:
+           return '\u001b[32mUNLOCKED\u001b[0m'
+
+#main/test code
+if __name__ == '__main__':
+    exDictionary = {
+        'john': 'fluffy',
+        'jan' : 'greg'
+        }
+    dictionary = dict_control(exDictionary)
+
+    dictionary.lock()
+    print(dictionary.access())
+    print(dictionary.stat())
+    dictionary.unlock()
+    print(dictionary.access())
+    print(dictionary.stat())
