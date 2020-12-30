08c800004b86e33255f5cb515866ab0ad9471437
shaunik.musukula <shaunik.musukula@gmail.com>
2020-12-29 18:55:21 -0800

    Deleted unnecessary directories.

--- venv/testingtest.py
+++
@@ -1,9 +0,0 @@
-def pass_encryption(x):
-    x = str(x.replace("a", "!"))
-    x = str(x.replace("b", "#"))
-    x = str(x.replace("c", "0"))
-    print(x)
-
-loop_start = "abc"
-
-pass_encryption(loop_start)
